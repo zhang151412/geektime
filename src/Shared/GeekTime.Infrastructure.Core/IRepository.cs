8db97ccb8186af7a2a096aff940e53c1af5aaf2a
Milo <zhang151412@163.com>
2021-04-18 17:09:44 +0800

    20210418 添加注释

--- src/Shared/GeekTime.Infrastructure.Core/IRepository.cs
+++ src/Shared/GeekTime.Infrastructure.Core/IRepository.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 
 namespace GeekTime.Infrastructure.Core
 {
+    /// <summary>
+    /// 包含普通实体的仓储
+    /// 约束 TEntity 必须是继承 Entity 的基类，必须实现聚合根 IAggregateRoot
+    /// 也就是说仓储里面存储的对象必须是一个聚合根对象
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public interface IRepository<TEntity> where TEntity : Entity, IAggregateRoot
     {
         IUnitOfWork UnitOfWork { get; }
@@ -14,11 +20,17 @@ namespace GeekTime.Infrastructure.Core
         Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
         TEntity Update(TEntity entity);
         Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
-        bool Remove(Entity entity);
+        bool Remove(Entity entity);// 由于没有指定主键，只能根据当前实体进行删除操作
         Task<bool> RemoveAsync(Entity entity);
     }
 
-
+    /// <summary>
+    /// 包含指定主键的类型的实体的仓储
+    /// 继承了上面的接口 IRepository<TEntity>，也就是说拥有了上面定义的所有方法
+    /// 另外一个，它实现了几个跟 Id 相关的操作的方法
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
     public interface IRepository<TEntity, TKey> : IRepository<TEntity> where TEntity : Entity<TKey>, IAggregateRoot
     {
         bool Delete(TKey id);
--- src/Shared/GeekTime.Infrastructure.Core/Repository.cs
+++ src/Shared/GeekTime.Infrastructure.Core/Repository.cs
@@ -1,14 +1,19 @@
 ﻿using GeekTime.Domain;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace GeekTime.Infrastructure.Core
 {
+    /// <summary>
+    /// 定义普通实体的仓储
+    /// 定义约束 TDbContext 必须是 EFContext，也就是仓储必须依赖于 EFContext 及其子类
+    /// 将来就可以把自己定义的比如 DomainContext 作为泛型参数传入 Repository，就可以很快捷地定义出来自己的仓储
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TDbContext"></typeparam>
     public abstract class Repository<TEntity, TDbContext> : IRepository<TEntity> where TEntity : Entity, IAggregateRoot where TDbContext : EFContext
     {
+        // 具体实现需要依赖 DbContext
         protected virtual TDbContext DbContext { get; set; }
 
         public Repository(TDbContext context)
@@ -16,7 +21,9 @@ namespace GeekTime.Infrastructure.Core
             this.DbContext = context;
         }
         public virtual IUnitOfWork UnitOfWork => DbContext;
+        // 因为 DbContext， EFContext 实际上实现了 IUnitOfWork，所以直接返回
 
+        // 下面这些方法都是 EntityFramework 提供的能力，所以就能通过简单的几行代码来实现基本的仓储操作
         public virtual TEntity Add(TEntity entity)
         {
             return DbContext.Add(entity).Entity;
@@ -49,13 +56,24 @@ namespace GeekTime.Infrastructure.Core
         }
     }
 
-
+    /// <summary>
+    /// 定义主键的实体的仓储
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TDbContext"></typeparam>
     public abstract class Repository<TEntity, TKey, TDbContext> : Repository<TEntity, TDbContext>, IRepository<TEntity, TKey> where TEntity : Entity<TKey>, IAggregateRoot where TDbContext : EFContext
     {
         public Repository(TDbContext context) : base(context)
         {
         }
-
+        /// <summary>
+        /// 根据 Id 从 DbContext 获取 Entity，然后再 Remove
+        /// 这样的好处是可以跟踪对象的状态
+        /// 坏处是任意的删除都需要先去数据库里面做查询
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public virtual bool Delete(TKey id)
         {
             var entity = DbContext.Find<TEntity>(id);
